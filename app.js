fbeba7a5034b03e3ab90fd38a98c35cd1f84b5f7
sagar104gurgaon <sagar104gurgaon@gmail.com>
2019-07-21 00:59:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const signup = require('./routes/signup');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/signup',signup);
+
+app.listen(3000,function(err){
+    console.log("listining on port 3000")
+})
